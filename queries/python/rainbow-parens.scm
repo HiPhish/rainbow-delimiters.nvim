e9f533be10484a2a7dcec5bf7c0315dcd64d3ffd
HiPhish <hiphish@posteo.de>
2023-01-11 20:19:19 +0100

    Add comprehension patterns to Python queries

--- queries/python/rainbow-parens.scm
+++ queries/python/rainbow-parens.scm
@@ -2,16 +2,40 @@
   (("[" @opening)
    ("]" @closing))) @container
 
+(list_comprehension
+  (("[" @opening)
+   (for_in_clause
+     (["for" "in"] @intermediate))
+   ("]" @closing))) @container
+
 (dictionary
   (("{" @opening)
+   ("}" @closing))) @container
+
+(dictionary_comprehension
+  (("{" @opening)
+   (for_in_clause
+     (["for" "in"] @intermediate))
    ("}" @closing))) @container
 
 (set
   (("{" @opening)
    ("}" @closing))) @container
 
+(set_comprehension
+  (("{" @opening)
+   (for_in_clause
+     (["for" "in"] @intermediate))
+   ("}" @closing))) @container
+
 (tuple
   (("(" @opening)
+   (")" @closing))) @container
+
+(generator_expression
+  (("(" @opening)
+   (for_in_clause
+     (["for" "in"] @intermediate))
    (")" @closing))) @container
 
 (argument_list
--- test/highlight/python/regular.py
+++ test/highlight/python/regular.py
@@ -3,5 +3,11 @@ my_dict = {'x': {'x': {'x': 'Hello, wold!'}}}
 my_set = {{{{'Hello, wold!'}}}}
 my_tuple = (((('Hello, wold!'),),),)
 
+list_comp = [i for i in [j for j in range(5)] if i % 2 == 0]
+dict_comp = {k: v for k, v in {k: v for k, v in {'k': 'v'}.items()}
+             if k == 'k'}
+set_comp = {i for i in {j for j in range(5)} if i % 2 == 0}
+gen_comp = (i for i in (j for j in range(5)) if i % 2 == 0)
+
 print(2 + ((((3)))))
 print(len(my_list))
