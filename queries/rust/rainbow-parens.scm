a66a59183bc96602b426c75b5c996330b817207d
CatThingy <andyc.andychen@gmail.com>
2023-01-14 12:31:24 -0800

    Add Rust query and test files

---
+++ queries/rust/rainbow-parens.scm
@@ -0,0 +1,91 @@
+(parenthesized_expression
+  (("(" @opening)
+   (")" @closing))) @container
+
+(declaration_list
+  (("{" @opening)
+   ("}" @closing))) @container
+
+(field_declaration_list
+  (("{" @opening)
+   ("}" @closing))) @container
+
+(ordered_field_declaration_list
+  (("(" @opening)
+   (")" @closing))) @container
+
+(enum_variant_list
+  (("{" @opening)
+   ("}" @closing))) @container
+
+(use_list
+  (("{" @opening)
+   ("}" @closing))) @container
+
+(field_initializer_list
+  (("{" @opening)
+   ("}" @closing))) @container
+
+(parameters
+ (("(" @opening)
+  (")" @closing))) @container
+
+(arguments
+ (("(" @opening)
+  (")" @closing))) @container
+
+(block
+  (("{" @opening)
+   ("}" @closing))) @container
+
+(match_block
+  (("{" @opening)
+   ("}" @closing))) @container
+
+(tuple_expression
+ (("(" @opening)
+  (")" @closing))) @container
+
+(token_tree
+  (("{" @opening)
+   ("}" @closing))) @container
+
+(token_tree
+  (("(" @opening)
+   (")" @closing))) @container
+
+(token_tree
+  (("[" @opening)
+   ("]" @closing))) @container
+
+(token_tree_pattern
+  (("(" @opening)
+   (")" @closing))) @container
+
+(token_repetition_pattern
+  (("(" @opening)
+   (")" @closing))) @container
+
+(attribute_item
+  (("[" @opening)
+   ("]" @closing))) @container
+
+(inner_attribute_item
+  (("[" @opening)
+   ("]" @closing))) @container
+
+(type_arguments
+ (("<" @opening)
+  (">" @closing))) @container
+
+(type_parameters
+ (("<" @opening)
+  (">" @closing))) @container
+
+(closure_parameters
+ (("|" @opening)
+  ("|" @closing))) @container
+
+(array_expression
+  (("[" @opening)
+   ("]" @closing))) @container
---
+++ test/highlight/rust/regular.rs
@@ -0,0 +1,132 @@
+#![crate_type = "lib"]
+struct NestedStruct {
+    value: u32,
+    inner: Inner,
+}
+
+struct Inner {
+    value: u32,
+}
+
+#[derive(Default, Debug)]
+struct TupleStruct(u32);
+
+#[cfg_attr(all(target_os = "linux", feature = "multithreaded"), derive(Default))]
+enum EnumTest {
+    TupleVariant(u32),
+    TupleVariantTupleStruct(TupleStruct),
+    StructVariant { value: u32 },
+    NestedStructVariant { inner: Inner },
+}
+
+fn test<A: Default>() -> usize {
+    std::mem::sizeof::<A>()
+}
+
+macro_rules! inefficient_vec {
+    ( $( $x:expr ),* ) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+    };
+}
+
+pub(crate) struct VisibilityModifier;
+
+fn main() {
+    let nested_vec: Vec<Vec<Vec<Vec<Vec<Vec<()>>>>>> = Vec::<_>::new();
+    let arr_arr_arr = [[[0; 4]; 4]; 4];
+    let constructed_struct = Inner { value: 0 };
+
+    let nested_constructed = NestedStruct {
+        value: 0,
+        inner: Inner { value: 0 },
+    };
+
+    let tuple_struct = TupleStruct(0);
+    let nested_tuple = (
+        ((1, 2, 3, 4), (5, 6, 7, 8)),
+        ((9, 10, 11, 12), (13, 14, 15, 16)),
+    );
+
+    let enums = vec![
+        EnumTest::TupleVariant(0),
+        EnumTest::TupleVariantTupleStruct(TupleStruct(0)),
+        EnumTest::StructVariant { value: 0 },
+        EnumTest::NestedStructVariant {
+            inner: Inner { value: 0 },
+        },
+    ];
+
+    let closure = |long_parameter_name: u8,
+                   long_parameter_name_two: u8,
+                   very_long_parameter_name: u8,
+                   extra_long_name: u8| {
+        let nested_closure = || {};
+        nested_closure();
+    };
+
+    let async_block = async { 0 };
+
+    let labelled_block = 'a: { 0 };
+
+    let boolean_expr = (((3 * 4) + 5) > 1 || false) && (true || true);
+
+    let num = 5;
+
+    let match_expr = match num {
+        1 | 2 | 3 => {}
+        | 4 
+        | 5 
+        | 6 => {}
+        7 => {}
+        8 => match boolean_expr {
+            _ => {}
+        },
+        _ => {}
+    };
+
+    let nested_macro = vec![vec![vec![vec![vec![0]]]]];
+}
+
+use level_1::{
+    level_2::{
+        level_3::{
+            level_4::{
+                level_5::{A, B},
+                C, D,
+            },
+            E, F,
+        },
+        G, H,
+    },
+    I, J,
+};
+
+mod level_1 {
+    pub mod level_2 {
+        pub mod level_3 {
+            pub mod level_4 {
+                pub mod level_5 {
+                    pub struct A;
+                    pub struct B;
+                }
+                pub struct C;
+                pub struct D;
+            }
+
+            pub struct E;
+            pub struct F;
+        }
+
+        pub struct G;
+        pub struct H;
+    }
+
+    pub struct I;
+    pub struct J;
+}
