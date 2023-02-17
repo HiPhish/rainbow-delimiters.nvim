d3579c33109854dd2b00c558ca89f5651e166798
Kim Gert Nielsen <kgn@one.com>
2023-02-17 15:27:26 +0100

    Add highlight for go

---
+++ queries/go/rainbow-parens.scm
@@ -0,0 +1,47 @@
+(import_spec_list
+   (("(" @opening)
+    (")" @closing))) @container
+
+(const_declaration
+   (("(" @opening)
+    (")" @closing))) @container
+
+(field_declaration_list
+   (("{" @opening)
+    ("}" @closing))) @container
+
+(argument_list
+   (("(" @opening)
+    (")" @closing))) @container
+
+(parameter_list
+   (("(" @opening)
+    (")" @closing))) @container
+
+(block
+   (("{" @opening)
+    ("}" @closing))) @container
+
+(expression_switch_statement
+   (("{" @opening)
+    ("}" @closing))) @container
+
+(type_switch_statement
+   (("{" @opening)
+    ("}" @closing))) @container
+
+(literal_value
+   (("{" @opening)
+    ("}" @closing))) @container
+
+(slice_type
+   (("[" @opening)
+    ("]" @closing))) @container
+
+(type_parameter_list
+   (("[" @opening)
+    ("]" @closing))) @container
+
+(type_arguments
+   (("[" @opening)
+    ("]" @closing))) @container
