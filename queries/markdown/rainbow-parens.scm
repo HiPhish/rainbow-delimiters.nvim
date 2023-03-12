f3ee0ab94348e3d20237f926d2290b5c406b1cf1
HiPhish <hiphish@posteo.de>
2023-03-12 16:48:50 +0100

    Add Markdown support

---
+++ queries/markdown/rainbow-parens.scm
@@ -0,0 +1,3 @@
+;;; Intentionally empty.  Markdown can have other languages embedded and we
+;;; want to let those grammars handle the highlighting.  This query only exists
+;;; to satisfy the requirements of this plugin.
