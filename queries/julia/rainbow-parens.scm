a6e8dd89f7d9f60352e9873c7c9dd2aaf987732b
zymon <s@zymon.org>
2023-04-02 12:28:26 +0200

    julia support + simple test

---
+++ queries/julia/rainbow-parens.scm
@@ -0,0 +1,23 @@
+(vector_expression
+  (("[" @opening)
+   ("]" @closing))) @container
+
+(matrix_expression
+  (("[" @opening)
+   ("]" @closing))) @container
+
+(parameter_list
+  (("(" @opening)
+   (")" @closing))) @container
+
+(argument_list
+  (("(" @opening)
+   (")" @closing))) @container
+
+(parenthesized_expression
+  (("(" @opening)
+   (")" @closing))) @container
+
+(comprehension_expression
+  (("[" @opening)
+   ("]" @closing))) @container
