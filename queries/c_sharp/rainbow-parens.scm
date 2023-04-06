e047076641fd4a71fb922e62155e8ba7bf4c0d6a
HiPhish <hiphish@posteo.de>
2023-04-07 00:37:46 +0200

    Remove redundant parentheses from C# query

--- queries/c_sharp/rainbow-parens.scm
+++ queries/c_sharp/rainbow-parens.scm
@@ -1,27 +1,27 @@
 (parenthesized_expression
-  (("(" @opening)
-   (")" @closing))) @container
+  "(" @opening
+  ")" @closing) @container
 
 (declaration_list
-  (("{" @opening)
-   ("}" @closing))) @container
+  "{" @opening
+  "}" @closing) @container
 
 (block
-  (("{" @opening)
-   ("}" @closing))) @container
+  "{" @opening
+  "}" @closing) @container
 
 (type_argument_list
- (("<" @opening)
-  (">" @closing))) @container
+  "<" @opening
+  ">" @closing) @container
 
 (initializer_expression
-  (("{" @opening)
-   ("}" @closing))) @container
+  "{" @opening
+  "}" @closing) @container
 
 (array_rank_specifier
-  (("[" @opening)
-   ("]" @closing))) @container
+  "[" @opening
+  "]" @closing) @container
 
 (bracketed_argument_list
-  (("[" @opening)
-   ("]" @closing))) @container
+  "[" @opening
+  "]" @closing) @container
