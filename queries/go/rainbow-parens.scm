35fc2536828326385911dfb76d9b9c7224df9808
HiPhish <hiphish@posteo.de>
2023-04-07 01:01:04 +0200

    Remove redundant parentheses from Go query

--- queries/go/rainbow-parens.scm
+++ queries/go/rainbow-parens.scm
@@ -1,63 +1,63 @@
 (parenthesized_expression
-  (("(" @opening)
-   (")" @closing))) @container
+  "(" @opening
+  ")" @closing) @container
 
 (import_spec_list
-   (("(" @opening)
-    (")" @closing))) @container
+  "(" @opening
+  ")" @closing) @container
 
 (var_declaration
-   (("(" @opening)
-    (")" @closing))) @container
+  "(" @opening
+  ")" @closing) @container
 
 (const_declaration
-   (("(" @opening)
-    (")" @closing))) @container
+  "(" @opening
+  ")" @closing) @container
 
 (field_declaration_list
-   (("{" @opening)
-    ("}" @closing))) @container
+  "{" @opening
+  "}" @closing) @container
 
 (argument_list
-   (("(" @opening)
-    (")" @closing))) @container
+  "(" @opening
+  ")" @closing) @container
 
 (parameter_list
-   (("(" @opening)
-    (")" @closing))) @container
+  "(" @opening
+  ")" @closing) @container
 
 (block
-   (("{" @opening)
-    ("}" @closing))) @container
+  "{" @opening
+  "}" @closing) @container
 
 (expression_switch_statement
-   (("{" @opening)
-    ("}" @closing))) @container
+  "{" @opening
+  "}" @closing) @container
 
 (type_switch_statement
-   (("{" @opening)
-    ("}" @closing))) @container
+  "{" @opening
+  "}" @closing) @container
 
 (literal_value
-   (("{" @opening)
-    ("}" @closing))) @container
+  "{" @opening
+  "}" @closing) @container
 
 (slice_type
-   (("[" @opening)
-    ("]" @closing))) @container
+  "[" @opening
+  "]" @closing) @container
 
 (map_type
-   (("[" @opening)
-    ("]" @closing))) @container
+  "[" @opening
+  "]" @closing) @container
 
 (interface_type
-   (("{" @opening)
-    ("}" @closing))) @container
+  "{" @opening
+  "}" @closing) @container
 
 (type_parameter_list
-   (("[" @opening)
-    ("]" @closing))) @container
+  "[" @opening
+  "]" @closing) @container
 
 (type_arguments
-   (("[" @opening)
-    ("]" @closing))) @container
+  "[" @opening
+  "]" @closing) @container
