1a54f2013bc1742444560d639d3cd22674e9d1d8
HiPhish <hiphish@posteo.de>
2023-04-07 00:35:30 +0200

    Remove redundant parentheses from C query

--- queries/c/rainbow-parens.scm
+++ queries/c/rainbow-parens.scm
@@ -1,30 +1,30 @@
 (parameter_list
-   (("(" @opening)
-    (")" @closing))) @container
+   "(" @opening
+   ")" @closing) @container
 
 (argument_list
-   (("(" @opening)
-    (")" @closing))) @container
+   "(" @opening
+   ")" @closing) @container
 
 (parenthesized_expression
-  (("(" @opening)
-   (")" @closing))) @container
+  "(" @opening
+  ")" @closing) @container
 
 (compound_statement
-  (("{" @opening)
-   ("}" @closing))) @container
+  "{" @opening
+  "}" @closing) @container
 
 (initializer_list
-  (("{" @opening)
-   ("}" @closing))) @container
+  "{" @opening
+  "}" @closing) @container
 
 (subscript_expression
-  (("[" @opening)
-   ("]" @closing))) @container
+  "[" @opening
+  "]" @closing) @container
 
 (field_declaration_list
-   (("{" @opening)
-    ("}" @closing))) @container
+   "{" @opening
+   "}" @closing) @container
 
 ;;; We could also add type casts, but those are not nested, so I don't think
 ;;; they belong here.
