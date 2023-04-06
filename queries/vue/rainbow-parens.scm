760f30182b3107edf672316753a3bdca8d1918d9
HiPhish <hiphish@posteo.de>
2023-04-07 01:43:39 +0200

    Remove redundant parentheses from Vue query

--- queries/vue/rainbow-parens.scm
+++ queries/vue/rainbow-parens.scm
@@ -1,35 +1,35 @@
 (element
-  ((start_tag
-      ["<" ">"] @opening)
-   (element
-      (self_closing_tag
-        ["<" "/>"] @intermediate))?
-   (end_tag
-      ["</" ">"] @closing))) @container
+  (start_tag
+    ["<" ">"] @opening)
+  (element
+    (self_closing_tag
+      ["<" "/>"] @intermediate))?
+  (end_tag
+    ["</" ">"] @closing)) @container
 
 (template_element
-  ((start_tag
-      ["<" ">"] @opening)
-   (element
-      (self_closing_tag
-        ["<" "/>"] @intermediate))?
-   (end_tag
-      ["</" ">"] @closing))) @container
+  (start_tag
+    ["<" ">"] @opening)
+  (element
+    (self_closing_tag
+      ["<" "/>"] @intermediate))?
+  (end_tag
+    ["</" ">"] @closing)) @container
 
 (script_element
-  ((start_tag
-      ["<" ">"] @opening)
-   (element
-      (self_closing_tag
-        ["<" "/>"] @intermediate))?
-   (end_tag
-      ["</" ">"] @closing))) @container
+  (start_tag
+    ["<" ">"] @opening)
+  (element
+    (self_closing_tag
+      ["<" "/>"] @intermediate))?
+  (end_tag
+    ["</" ">"] @closing)) @container
 
 (style_element
-  ((start_tag
-      ["<" ">"] @opening)
-   (element
-      (self_closing_tag
-        ["<" "/>"] @intermediate))?
-   (end_tag
-      ["</" ">"] @closing))) @container
+  (start_tag
+    ["<" ">"] @opening)
+  (element
+    (self_closing_tag
+      ["<" "/>"] @intermediate))?
+  (end_tag
+    ["</" ">"] @closing)) @container
