51985b2042fcf090102cbb52019d8daa811593f5
HiPhish <hiphish@posteo.de>
2023-04-07 01:21:40 +0200

    Remove redundant parentheses from Python query

--- queries/python/rainbow-parens.scm
+++ queries/python/rainbow-parens.scm
@@ -1,51 +1,51 @@
 (list
-  (("[" @opening)
-   ("]" @closing))) @container
+  "[" @opening
+  "]" @closing) @container
 
 (list_comprehension
-  (("[" @opening)
-   (for_in_clause
-     (["for" "in"] @intermediate))
-   ("]" @closing))) @container
+  "[" @opening
+  (for_in_clause
+    ["for" "in"] @intermediate)
+  "]" @closing) @container
 
 (dictionary
-  (("{" @opening)
-   ("}" @closing))) @container
+  "{" @opening
+  "}" @closing) @container
 
 (dictionary_comprehension
-  (("{" @opening)
-   (for_in_clause
-     (["for" "in"] @intermediate))
-   ("}" @closing))) @container
+  "{" @opening
+  (for_in_clause
+    ["for" "in"] @intermediate)
+  "}" @closing) @container
 
 (set
-  (("{" @opening)
-   ("}" @closing))) @container
+  "{" @opening
+  "}" @closing) @container
 
 (set_comprehension
-  (("{" @opening)
-   (for_in_clause
-     (["for" "in"] @intermediate))
-   ("}" @closing))) @container
+  "{" @opening
+  (for_in_clause
+    ["for" "in"] @intermediate)
+  "}" @closing) @container
 
 (tuple
-  (("(" @opening)
-   (")" @closing))) @container
+  "(" @opening
+  ")" @closing) @container
 
 (generator_expression
-  (("(" @opening)
-   (for_in_clause
-     (["for" "in"] @intermediate))
-   (")" @closing))) @container
+  "(" @opening
+  (for_in_clause
+    ["for" "in"] @intermediate)
+  ")" @closing) @container
 
 (argument_list
-  (("(" @opening)
-   (")" @closing))) @container
+  "(" @opening
+  ")" @closing) @container
 
 (parenthesized_expression
-  (("(" @opening)
-   (")" @closing))) @container
+  "(" @opening
+  ")" @closing) @container
 
 (subscript
-  (("[" @opening)
-   ("]" @closing))) @container
+  "[" @opening
+  "]" @closing) @container
