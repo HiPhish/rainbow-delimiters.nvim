3a248fb11ac44e83388c8646b687b3bd78648d76
HiPhish <hiphish@posteo.de>
2023-04-07 01:29:59 +0200

    Remove redundant parentheses from Regex query

--- queries/regex/rainbow-parens.scm
+++ queries/regex/rainbow-parens.scm
@@ -1,17 +1,17 @@
 (anonymous_capturing_group
-  ("(" @opening)
-  (")" @closing)) @container
+  "(" @opening
+  ")" @closing) @container
 
-;;; The inversion `^` is an optional part of the delimiter
+;;; The inversion `^` should be an opening node as well
 (character_class
-  (["[" "^"?] @opening)
-  ("]" @closing)) @container
+  "[" @opening
+  "]" @closing) @container
 
 (count_quantifier
-  ("{" @opening)
-  ("}" @closing)) @container
+  "{" @opening
+  "}" @closing) @container
 
 ;;; We should probably include the character after `?` like `=` as well
 (lookahead_assertion
-  ("(?" @opening)
-  (")" @closing)) @container
+  "(?" @opening
+  ")" @closing) @container
