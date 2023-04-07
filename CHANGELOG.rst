957235a658ccc17195a9030176c2e66095cea64f
HiPhish <hiphish@posteo.de>
2023-04-07 14:14:25 +0200

    Remove redundant parentheses from Dart query

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -15,6 +15,7 @@ Unreleased
 Added
 =====
 
+- Support for Dart
 - Compatibility with Tree-sitter API in Neovim 0.9
 - Support for R (`r`)
 - Go queries for nodes `interface_type`, `map_type` and `var_declaration`
--- queries/dart/rainbow-parens.scm
+++ queries/dart/rainbow-parens.scm
@@ -1,31 +1,31 @@
 (block
-  (("{" @opening)
-   ("}" @closing))) @container
+  "{" @opening
+  "}" @closing) @container
 
 (arguments
-  (("(" @opening)
-   (")" @closing))) @container
+  "(" @opening
+  ")" @closing) @container
 
 (class_body
-  (("{" @opening)
-   ("}" @closing))) @container
+  "{" @opening
+  "}" @closing) @container
 
 (formal_parameter_list
-  (("(" @opening)
-   (")" @closing))) @container
+  "(" @opening
+  ")" @closing) @container
 
 (optional_formal_parameters
-  (("{" @opening)
-   ("}" @closing))) @container
+  "{" @opening
+  "}" @closing) @container
 
 (list_literal
-  (("[" @opening)
-   ("]" @closing))) @container
+  "[" @opening
+  "]" @closing) @container
 
 (set_or_map_literal
-  (("{" @opening)
-   ("}" @closing))) @container
+  "{" @opening
+  "}" @closing) @container
 
 (type_arguments
-  (("<" @opening)
-   (">" @closing))) @container
+  "<" @opening
+  ">" @closing) @container
