93fa50e5fbfa188d7552388a2290b9290c202d4b
HiPhish <hiphish@posteo.de>
2023-06-18 15:54:10 +0200

    Add test to ensure buffers are only attached once

--- lua/ts-rainbow/strategy/track.lua
+++ lua/ts-rainbow/strategy/track.lua
@@ -1,22 +1,27 @@
 ---A strategy decorator; the decorated strategy keeps track of all currently
 ---attached buffers.  Thew new strategy has the following fields:
 ---
---- - strategy  The wrapped strategy object
---- - buffers   Table mapping of buffer number to buffer settings
+--- - strategy     The wrapped strategy object
+--- - buffers      Table mapping of buffer number to buffer settings
+--- - attachments  Number of currently active attachments
 ---
 ---@param strategy table
 local function track(strategy)
 	local buffers = {}
+	local attachments = {0}  -- Table because want to pass it by reference
 
 	return {
 		strategy = strategy,
 		buffers = buffers,
+		attachments = attachments,
 		on_attach = function(bufnr, settings, ...)
 			buffers[bufnr] = settings
-			strategy.on_attach(bufnr, settings, ...)
+			attachments[1] = attachments[1] + 1
+			print(attachments, vim.inspect(attachments))
 		end,
 		on_detach = function(bufnr, ...)
 			buffers[bufnr] = nil
+			attachments[1] = attachments[1] - 1
 			strategy.on_detach(bufnr, ...)
 		end
 	}
