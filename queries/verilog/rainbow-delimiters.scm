65607b06455e1a53b933e60a386587ed65d7738a
Matt Young <matt.young.1@outlook.com>
2023-07-05 16:55:57 +1000

    Initial support for Verilog and SystemVerilog

---
+++ queries/verilog/rainbow-delimiters.scm
@@ -0,0 +1,60 @@
+(seq_block
+    "begin" @opening
+    "end" @closing
+) @container
+
+(packed_dimension
+    "[" @opening
+    "]" @closing
+) @container
+
+(data_type
+    "{" @opening
+    "}" @closing
+) @container
+
+(named_port_connection
+    "(" @opening
+    ")" @closing
+) @container
+
+(named_parameter_assignment
+    "(" @opening
+    ")" @closing
+) @container
+
+(hierarchical_instance
+    "(" @opening
+    ")" @closing
+) @container
+
+(parameter_value_assignment
+    "(" @opening
+    ")" @closing
+) @container
+
+(parameter_port_list
+    "(" @opening
+    ")" @closing
+) @container
+
+(list_of_port_declarations
+    "(" @opening
+    ")" @closing
+) @container
+
+(cast
+    "(" @opening
+    ")" @closing
+) @container
+
+(conditional_statement
+    "(" @opening
+    ")" @closing
+) @container
+
+(event_control
+    "(" @begin
+    ")" @end
+) @container
+
---
+++ test/highlight/verilog/systemverilog.sv
@@ -0,0 +1,61 @@
+// Comment
+`timescale 1ns/1ns
+`default_nettype none
+`include "filename.svh"
+
+module test #(
+    parameter int PARAM = 1
+) (
+    input logic[15:0] a,
+    input logic[15:0] b,
+
+    output logic[15:0] c
+);
+    logic[15:0][8:0] packed_data;
+
+    always_comb begin
+        if (a > b) begin
+            c = 15'd1;
+        end else if ((a == b) || (a == c)) begin
+            c = 15'd2;
+
+            if (c == 15'd2) begin
+                c = 15'd3;
+                packed_data[2][3] = 8'd4;
+            end
+        end
+    end
+
+    always_ff @(posedge a) begin
+        c <= ((a + b) + b);
+    end
+endmodule
+
+module test2 #(
+    parameter int PARAM = 1,
+    parameter int PARAM2 = ((2+2) / (4+4))
+) (
+    input logic[15:0] a,
+    input logic[15:0] b,
+
+    output logic[15:0] c
+);
+    logic[15:0][8:0] packed_data;
+
+    always_comb begin
+        if (a > b) begin
+            c = 15'd1;
+        end else if ((a == b) || (a == c)) begin
+            c = 15'd2;
+
+            if (c == 15'd2) begin
+                c = 15'd3;
+                packed_data[2][3] = $clog2($clog2(24));
+            end
+        end
+    end
+
+    always_ff @(posedge a) begin
+        c <= ((a + b) + b);
+    end
+endmodule
