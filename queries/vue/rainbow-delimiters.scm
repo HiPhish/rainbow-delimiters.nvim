93dfe1eb1f15a1eb776a545b4f978e577570e008
Zhanibek Adilbekov <zhanibek.adilbekov@proton.me>
2023-08-09 17:03:42 +0600

    feat(vue): highlight only tag names

--- queries/vue/rainbow-delimiters.scm
+++ queries/vue/rainbow-delimiters.scm
@@ -2,17 +2,20 @@
 ;;; languages
 
 (element
-  (start_tag) @opening
-  (end_tag)   @closing) @container
+  (start_tag (tag_name) @opening)
+  (end_tag (tag_name)   @closing)) @container
+
+(element
+  (self_closing_tag (tag_name) @opening)) @container
 
 (template_element
-  (start_tag) @opening
-  (end_tag)   @closing) @container
+  (start_tag (tag_name) @opening)
+  (end_tag (tag_name)   @closing)) @container
 
 (script_element
-  (start_tag) @opening
-  (end_tag)   @closing) @container
+  (start_tag (tag_name) @opening)
+  (end_tag (tag_name)   @closing)) @container
 
 (style_element
-  (start_tag) @opening
-  (end_tag)   @closing) @container
+  (start_tag (tag_name) @opening)
+  (end_tag (tag_name)   @closing)) @container
