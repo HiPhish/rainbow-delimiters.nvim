e75a8d5914d5518f76cf904f3258e032b45d863b
Zhanibek Adilbekov <zhanibek.adilbekov@proton.me>
2023-08-09 17:22:28 +0600

    feat(sql): add support for sql

---
+++ queries/sql/rainbow-delimiters.scm
@@ -0,0 +1,19 @@
+(term
+    "(" @opening
+    ")" @closing) @container
+
+(subquery
+    "(" @opening
+    ")" @closing) @container
+
+(invocation
+    "(" @opening
+    ")" @closing) @container
+
+(list
+    "(" @opening
+    ")" @closing) @container
+
+(binary_expression
+    "(" @opening
+    ")" @closing) @container
