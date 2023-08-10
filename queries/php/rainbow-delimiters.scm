9b872019b3b501d330421b99dd0a34d233205184
HiPhish <hiphish@posteo.de>
2023-08-10 12:53:07 +0200

    Comment out string interpolation in PHP

--- queries/php/rainbow-delimiters.scm
+++ queries/php/rainbow-delimiters.scm
@@ -18,9 +18,10 @@
     "{" @opening
     "}" @closing) @container
 
-(encapsed_string
-    "{" @opening
-    "}" @closing) @container
+;; Commented out until https://github.com/neovim/neovim/pull/17099 is resolved
+; (encapsed_string
+;     "{" @opening
+;     "}" @closing) @container
 
 (array_creation_expression
     "[" @opening
