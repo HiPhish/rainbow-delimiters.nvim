f86a3bcaff6ec8f2bf24304e233ad54659686657
HiPhish <hiphish@posteo.de>
2023-08-16 20:52:50 +0200

    Fix C++ query
    The C++ grammar has deviated from the C grammar.

--- queries/cpp/rainbow-delimiters.scm
+++ queries/cpp/rainbow-delimiters.scm
@@ -1,4 +1,32 @@
-; inherits: c
+;;; NOTE: The C and C++ grammar have diverged, so I cannot include the C query.
+
+(parameter_list
+   "(" @opening
+   ")" @closing) @container
+
+(argument_list
+   "(" @opening
+   ")" @closing) @container
+
+(parenthesized_expression
+  "(" @opening
+  ")" @closing) @container
+
+(compound_statement
+  "{" @opening
+  "}" @closing) @container
+
+(initializer_list
+  "{" @opening
+  "}" @closing) @container
+
+(subscript_argument_list
+  "[" @opening
+  "]" @closing) @container
+
+(field_declaration_list
+   "{" @opening
+   "}" @closing) @container
 
 (declaration_list
   "{" @opening
