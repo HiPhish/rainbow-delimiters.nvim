6ca31c04b92efa0400ecbb049cfbe17ecf63aaff
Charlotte Smith <charliesmith5019@gmail.com>
2023-09-10 20:07:50 +0100

    feat(fsharp): add fsharp support

---
+++ queries/fsharp/rainbow-delimiters.scm
@@ -0,0 +1,87 @@
+; ─── Disabled due to being unable to match more than one argument at a time ───
+;
+;(argument_patterns
+;  ("(" @opening ")" @closing)+
+;  ) @container
+;
+;(record_type_defn
+;  "{" @opening
+;  "}" @closing
+;  ) @container
+;
+;(type_arguments
+;  "<" @opening
+;  ">" @closing
+;  ) @container
+;
+
+; highlight unit expressions in function applications 
+; and return types for consistency
+(application_expression
+  (const 
+    (unit
+      "(" @opening
+      ")" @closing
+    )) @container
+  )
+
+(return_expression
+  (const 
+    (unit
+      "(" @opening
+      ")" @closing
+    )) @container
+  )
+
+(if_expression
+  (const 
+    (unit
+      "(" @opening
+      ")" @closing
+    )) @container
+  )
+
+(function_or_value_defn 
+  body: (const 
+    (unit
+      "(" @opening
+      ")" @closing
+    )) @container
+ )
+
+(paren_expression
+  "(" @opening
+  ")" @closing
+  ) @container
+
+(paren_pattern
+  "(" @opening
+  ")" @closing
+  ) @container
+
+(type
+  "(" @opening
+  ")" @closing
+  ) @container
+
+(type
+  "<" @opening
+  ">" @closing
+  ) @container
+
+
+(list_pattern
+  "[" @opening
+  "]" @closing
+  ) @container
+
+(array_pattern
+  "[|" @opening
+  "|]" @closing
+  ) @container
+
+(ce_expression
+  (long_identifier_or_op (long_identifier)) @intermediate
+  "{" @opening
+  "}" @closing
+  ) @container
