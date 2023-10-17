b160af1d2a2a616718c4b01cd88105a8f4c88a73
HiPhish <hiphish@posteo.de>
2023-10-17 23:15:06 +0200

    Allow calling the setup module directly

--- lua/rainbow-delimiters/setup.lua
+++ lua/rainbow-delimiters/setup.lua
@@ -8,6 +8,10 @@ function M.setup(opts)
 	vim.g.rainbow_delimiters = opts
 end
 
+
+-- Make it possible to call the module directly; for backwards compatibility
+-- with a previous version of this module.
+setmetatable(M, {__call = M.setup})
 return M
 
 -- vim:tw=79:ts=4:sw=4:noet:
