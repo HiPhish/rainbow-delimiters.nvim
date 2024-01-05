3f33fb67286d97d904fb4ddd5e477a25488dca0e
kawre <kenterq@gmail.com>
2024-01-05 18:54:21 +0100

    feat: add missing `java` delimiters

--- queries/java/rainbow-delimiters.scm
+++ queries/java/rainbow-delimiters.scm
@@ -6,7 +6,27 @@
   "{" @delimiter
   "}" @delimiter @sentinel) @container
 
+(array_initializer
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
 (formal_parameters
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(condition
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(catch_clause 
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(parenthesized_expression 
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(inferred_parameters 
   "(" @delimiter
   ")" @delimiter @sentinel) @container
 
@@ -14,7 +34,23 @@
   "(" @delimiter
   ")" @delimiter @sentinel) @container
 
+(for_statement
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(enhanced_for_statement 
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(constructor_body
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
 (dimensions
+  "[" @delimiter
+  "]" @delimiter @sentinel) @container
+
+(dimensions_expr
   "[" @delimiter
   "]" @delimiter @sentinel) @container
 
