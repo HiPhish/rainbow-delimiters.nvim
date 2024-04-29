2279491726e827bf8eb9e32853db6540a506ce2d
Alexei Mozaidze <lexerpexer@proton.me>
2024-04-29 13:34:16 +0400

    fix(fennel): update queries
    Updated the queries to support the updated upstream parser. Also, simplified queries significantly.

--- queries/fennel/rainbow-delimiters.scm
+++ queries/fennel/rainbow-delimiters.scm
@@ -1,16 +1,3 @@
-(list
-  "(" @delimiter
-  ")" @delimiter @sentinel) @container
-
-(sequence
-  "[" @delimiter
-  "]" @delimiter @sentinel) @container
-
-(table
-  "{" @delimiter
-  ;; NOTE: see example file for usecase
-  ;; item:
-  ;;   (table_pair
-  ;;      key: (symbol) @delimiter @_colon
-  ;;      (#eq? @_colon ":"))*
-  "}" @delimiter @sentinel) @container
+(_
+  open: ["(" "[" "{"] @delimiter
+  close: [")" "]" "}"] @delimiter @sentinel) @container
