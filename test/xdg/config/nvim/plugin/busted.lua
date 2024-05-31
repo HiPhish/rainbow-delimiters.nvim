0f314613125fdd7c85faaee3c87051611d883253
HiPhish <hiphish@posteo.de>
2024-05-31 21:06:07 +0200

    Extend `has_extmarks_at` test assertion
    The assertion now takes an extra parameter to check for a specific
    highlight group.  By default any highlight group will match, but if one
    is specified only extmarks with that particular highlight groups are
    counted.
    
    While I was at it I also noticed that the `vim.fn.filter` function was
    used wrongly, it did not mutate the Lua list in place.  I had to replace
    it with `vim.tbl_filter`.

--- test/xdg/config/nvim/plugin/busted.lua
+++ test/xdg/config/nvim/plugin/busted.lua
@@ -2,27 +2,43 @@
 
 local say = require 'say'
 local assert = require 'luassert'
-local filter = vim.fn.filter
 
 
+---Globally unique key to retrieve the current Neovim channel from the test
+---state.
 local NVIM_STATE_KEY = {}
 
-local function nvim(state, args, level)
+local function nvim_client(state, args, _level)
 	assert(args.n > 0, "No Neovim channel provided to the modifier")
 	assert(rawget(state, NVIM_STATE_KEY) == nil, "Neovim already set")
 	rawset(state, NVIM_STATE_KEY, args[1])
 	return state
 end
 
----Asserts that there are Rainbow Delimiters extmarks at the given position
+---Asserts that there are Rainbow Delimiters extmarks at the given position.
+---
+---Arguments:
+---
+---  1) row        Zero-indexed row of the character position
+---  2) column     Zero-indexed column of the character position
+---  3) lang       Expected language of the extmark
+---  4) hl_group   Optional, expected highlight group of the extmark
+---
 ---@param arguments integer[]  Row and column, both zero-based
-local function has_extmarks_at(_state, arguments, lang)
+---@return boolean
+local function has_extmarks_at(_state, arguments)
 	local nvim = rawget(_state, NVIM_STATE_KEY)
 	assert(nvim ~= nil, 'No Neovim channel set, use the nvim modifier to set the channel')
-	local row, column = arguments[1], arguments[2]
+	local row, column, lang, hl_group = arguments[1], arguments[2], arguments[3], arguments[4]
 	local nsid = nvim:exec_lua('return require("rainbow-delimiters.lib").nsids[...]', {lang})
+
 	local extmarks = nvim:exec_lua('return vim.inspect_pos(...).extmarks', {0, row, column})
-	filter(extmarks, function(_, v) return v.ns_id == nsid end)
+	extmarks = vim.tbl_filter(function(v) return v.ns_id == nsid end, extmarks)
+	if hl_group then
+		local function same_hl_group(extmark) return extmark.opts.hl_group == hl_group end
+		extmarks = vim.tbl_filter(same_hl_group, extmarks)
+	end
+
 	return #extmarks > 0
 end
 
@@ -32,4 +48,4 @@ say:set('assertion.extmarks_at.negative', 'Expected no extmarks at (%s, %s)')
 assert:register(
 	'assertion', 'has_extmarks_at', has_extmarks_at,
 	'assertion.has_extmarks_at.positive', 'assertion.has_extmarks_at.negative')
-assert:register('modifier', 'nvim', nvim)
+assert:register('modifier', 'nvim', nvim_client)
