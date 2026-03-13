7c183b647576834a32bdc1450bbba0dbdcea6125
Matteo Di Rienzo <144356345+Matteo-Di-Rienzo@users.noreply.github.com>
2026-03-13 11:57:49 -0400

    Add bracket query to block query

--- queries/elixir/rainbow-blocks.scm
+++ queries/elixir/rainbow-blocks.scm
@@ -1,3 +1,38 @@
+(call
+  (arguments
+    "(" @delimiter
+    ")" @delimiter @sentinel) @container)
+
+(block
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(string
+  (interpolation
+    "#{" @delimiter
+    "}" @delimiter @sentinel) @container)
+
+(tuple
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
+(list
+  "[" @delimiter
+  "]" @delimiter @sentinel) @container
+
+(access_call
+  "[" @delimiter
+  "]" @delimiter @sentinel) @container
+
+(bitstring
+  "<<" @delimiter
+  ">>" @delimiter @sentinel) @container
+
+(map
+  "%" @delimiter
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
 (do_block
   "do" @delimiter
   "end" @delimiter) @container
