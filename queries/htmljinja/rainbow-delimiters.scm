6dc4082a22588d719a75f60c1e0aad1c1bc81720
Tim Keller <tjk@tjkeller.xyz>
2026-09-02 16:53:16 -0500

    Add REUSE license header to htmljinja/rainbow-delimiters.scm

--- queries/htmljinja/rainbow-delimiters.scm
+++ queries/htmljinja/rainbow-delimiters.scm
@@ -1,3 +1,5 @@
+;; SPDX-License-Identifier: 0BSD
+;; SPDX-FileCopyrightText: © 2026 tjkeller-xyz
 ;;; Intentionally empty.  A Jinja template contains HTML, and this file exists
 ;;; so that the HTML nodes can be highlighted.  This query only exists to
 ;;; satisfy the requirements of this plugin.
